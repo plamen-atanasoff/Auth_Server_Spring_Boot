205b7d408d02b9f4ca205a4ddcd58b287b3d90d9
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-08-19 16:32:54 +0300

    Initial commit

---
+++ src/main/java/com/Auth_Server_Spring_Boot/AuthServerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.Auth_Server_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Auth_Server_Spring_Boot/AuthServerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Auth_Server_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
